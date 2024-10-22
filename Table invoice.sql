f8a909f8a2778f706ef1211a4f351b187c77815b
Iryna Panchenko <iryna.panchenko@lnu.edu.ua>
2024-10-22 10:49:28 +0300

    Table invoice.sql

--- Table invoice.sql
+++ Table invoice.sql
@@ -1,12 +1,12 @@
 USE company;
 
 CREATE TABLE IF NOT EXISTS `invoice` (
-`invoice_id` BIGINT(15) NOT NULL,
-`employee_id` INT NOT NULL,
-`customer_id` INT,
-`payment_method` INT NOT NULL,
-`transaction_moment` DATETIME NOT NULL,
-`status` varchar(10) NOT NULL
+	`invoice_id` BIGINT(15) NOT NULL,
+    `employee_id` INT NOT NULL,
+    `customer_id` INT,
+    `payment_method` INT NOT NULL,
+    `transaction_moment` DATETIME NOT NULL,
+    `status` varchar(10) NOT NULL
 );
 
 -- Add new columns
@@ -21,13 +21,16 @@ ALTER TABLE orders DROP amount;
 ALTER TABLE orders DROP customer_id;
 ALTER TABLE orders DROP employee_id;
 
+
 -- Modify column
 ALTER TABLE customer MODIFY phone_number BIGINT(15) NOT NULL;
 ALTER TABLE employee MODIFY position VARCHAR(30) NOT NULL;
 
 -- Modify department
-ALTER TABLE department MODIFY street varchar(50) NOT NULL;
-ALTER TABLE department MODIFY building_no INT(4);
+ALTER TABLE department_ MODIFY street varchar(50) NOT NULL;
+ALTER TABLE department_ MODIFY building_no INT(4);
+ALTER TABLE employee MODIFY first_name varchar(30);
+ALTER TABLE customer MODIFY birth_date DATE;
 
 -- Primary key 
 ALTER TABLE invoice ADD PRIMARY KEY (invoice_id);
@@ -37,9 +40,10 @@ ALTER TABLE orders DROP FOREIGN KEY orders_fk1;
 ALTER TABLE orders DROP FOREIGN KEY orders_fk3;
 ALTER TABLE orders ADD CONSTRAINT orders_fk2 FOREIGN KEY (product_id) REFERENCES product(product_id);
 ALTER TABLE invoice ADD CONSTRAINT invoice_fk0 FOREIGN KEY (employee_id) REFERENCES employee(employee_id);
+ALTER TABLE orders ADD CONSTRAINT orders_fk10 FOREIGN KEY (invoice_id) REFERENCES invoice(invoice_id);
 ALTER TABLE invoice ADD CONSTRAINT invoice_fk1 FOREIGN KEY (customer_id) REFERENCES customer(customer_id);
  
  
  -- UNIQUE Constraint
- ALTER TABLE department ALTER city SET DEFAULT 'Lviv';
+ ALTER TABLE department_ ALTER city SET DEFAULT 'Lviv';
  ALTER TABLE employee ADD UNIQUE (user_name);
